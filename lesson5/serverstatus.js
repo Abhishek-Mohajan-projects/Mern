c2d55ff2edae0c11c1ea056819027132f251a7c6
Abhishek-Mohajan-projects <avimm079@gmail.com>
2023-07-18 21:04:17 +0600

    http,res,req model and status code

--- lesson5/serverstatus.js
+++ lesson5/serverstatus.js
@@ -3,7 +3,7 @@ const port = 3000;
 const hostname = "127.0.0.1";
 
 const myServer = http.createServer((req, res) => {
-  res.writeHead(204, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
   /* res.writeHead(202, { "Content-Type": "text/html" }); */
   res.write("node js");
   res.end("<h1>Hello node js</h1>");
